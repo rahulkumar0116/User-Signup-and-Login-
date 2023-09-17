6aefeea2069ce53f71e3771c0296560733bc39fd
Rahul Kumar <rahulkumar0116@gmail.com>
2023-09-17 14:01:51 +0530

    middleware Add..

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation"
+import { toast } from "react-hot-toast";
+
 
 const HomePage = () => {
+
+  const router = useRouter();
+  
+  const Logout = async ()=>{
+  try {
+    await axios.get("/api/user/logout")
+    toast.success("Logout successful")
+    router.push("/login")
+  } catch (error) {
+    console.log(error.message)
+    toast.error(error.message)
+  }
+}
   return (
-    <div className=' flex justify-center'>profile is Here</div>
+    <div className=' flex flex-col itams-center justify-center'>
+      <h2>Profile</h2>
+      <hr />
+      <p>Profile Page</p>
+      <hr />
+      <button onClick={Logout}>Logout</button>
+    </div>
+    
   )
 }
 
